4e84cbef179fca11a3e405c4ac6379b0d4d03a39
Dihfahsih1 <dihfahsihm@gmail.com>
2020-04-11 18:48:38 +0300

    passing messages in the chat as property

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
 import './Messages.css';
-const Input = ({message, setMessage, sendMessage }) =>(
-
+const Messages = (messages, name) =>(
+  <ScrollToBottom>
+    {messages.map((message, i)=><div key={i}><Message message={message} name={name} /></div>)}
+  </ScrollToBottom>
 )
-export default Input;
+export default Messages;
