60ce5730686ab06a9cb3114a5af5db7f1ed62d38
Dihfahsih1 <dihfahsihm@gmail.com>
2020-04-13 23:51:31 +0300

    include emojis

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import './Message.css';
+import ReactEmoji from 'react-emoji';
 const Message = ({message: {user, text}, name}) =>{
   let isSentByCurrentUser = false;
 
-  const trimmedName = name.trim().toLowerCase();
+  //const trimmedName = name.trim().toLowerCase();
 
-  if(user === trimmedName){
+  if(user === user){
     isSentByCurrentUser = true;
   }
   return(
     isSentByCurrentUser
     ? (
       <div className="messageContainer justifyEnd">
-        <p className="sentText pr-20"> {trimmedName} </p>
+        <p className="sentText pr-20"> {user} </p>
         <div className="messageBox backgroundBlue">
-          <p className="messageText colorWhite"> {text} </p>
+          <p className="messageText colorWhite"> {ReactEmoji.emojify(text)} </p>
         </div>
       </div>
     )
     :(
       <div className="messageContainer justifyStart">
         <div className="messageBox backgroundLight">
-          <p className="messageText colorDark"> {text} </p>
+          <p className="messageText colorDark"> {ReactEmoji.emojify(text)} </p>
         </div>
           <p className="sentText pl-10"> {user} </p>
       </div>
